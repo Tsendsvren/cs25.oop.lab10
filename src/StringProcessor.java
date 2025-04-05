d301f5855b4d60a6bdc6ea88a8197cc7191a12fe
Tsendsvren <tsekvv2006@gmail.com>
2025-04-05 17:04:24 +0800

    Dasgal 3 duussan

--- src/StringProcessor.java
+++ src/StringProcessor.java
@@ -6,11 +6,27 @@ public class StringProcessor {
 			Predicate<String> filter,
 			Function<String, String> transformer,
 			Consumer<String> printer) {
-		// Хэрэгжүүл: filter, transform, print хий
+		for (String str : strings) {
+			if (filter.test(str)) {
+				String transformed = transformer.apply(str);
+				printer.accept(transformed);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
-		// Хэрэгжүүл: Predicate, Function, Consumer-ийг тодорхойлж, processStrings дууд
+		List<String> words = Arrays.asList("Java", "Programmchlal", "Code");
+
+		// Predicate: 5 тэмдэгтээс урт үгсийг шүүх
+		Predicate<String> longerThan5 = s -> s.length() > 5;
+
+		// Function: үгийг урвуул
+		Function<String, String> reverse = s -> new StringBuilder(s).reverse().toString();
+
+		// Consumer: том үсгээр, "ГАРШИЛ: " угтвартай хэвлэх
+		Consumer<String> printHeadline = s -> System.out.println("GARSHIL: " + s.toUpperCase());
+
+		// Уг функцуудыг ашиглан processStrings дууд
+		processStrings(words, longerThan5, reverse, printHeadline);
 	}
 }
